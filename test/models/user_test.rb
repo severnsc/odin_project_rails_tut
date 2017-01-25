b7631e4a6d56c584acfc68d92abfad847e1f6313
Chris Severns <severnsc@gmail.com>
2017-01-25 16:31:52 -0500

    Save emails to DB as all lower case

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,4 +52,11 @@ class UserTest < ActiveSupport::TestCase
   	@user.save
   	assert_not duplicate_user.valid?
   end
+
+  test "email addresses should be saved as all lower case" do
+  	mixed_case_email = "FoO@ExAmPlE.CoM"
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.email
+  end
 end
