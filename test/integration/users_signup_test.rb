b7ace230de99c771b10483c0363670cdc007373a
Chris Severns <severnsc@gmail.com>
2017-01-30 15:57:27 -0500

    test for valid submission

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	assert_select 'div#error_explanation'
   	assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_select "form[action='/signup']"
+    assert_difference 'User.count' do
+      post signup_path, params: { user: { name: "Example User2",
+                                          email: "user@example.com",
+                                          password: "mynewpassword",
+                                          password_confirmation: "mynewpassword"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
+  end
 end
