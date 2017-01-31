486092f2418562e3546e6be9b6dc784e4cd403be
Chris Severns <severnsc@gmail.com>
2017-01-31 16:17:43 -0500

    Add logic for successful editing of user information

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 					  format: { with: VALID_EMAIL_REGEX},
 					  uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password, length: {minimum: 8}, presence: true
+	validates :password, length: {minimum: 8}, presence: true, allow_nil: true
 
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
