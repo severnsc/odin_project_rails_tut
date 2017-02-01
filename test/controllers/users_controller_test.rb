a4d073521d1d207c7de7130e6c6fd23a6a54fb0d
Chris Severns <severnsc@gmail.com>
2017-02-01 15:56:48 -0500

    Add test to ensure admin status cannot be changed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,4 +44,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 	assert_redirected_to root_url  										   
   end
 
+  test "should not allow the admin attribute to be editable" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user:{password: "",
+                                                  password_confirmation: "",
+                                                  admin: true}}
+    assert_not @other_user.admin?                                                  
+  end
+
 end
