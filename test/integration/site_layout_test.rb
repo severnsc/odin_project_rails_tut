6271f1ed74426c4284ab6e08714c59f38eecae60
Chris Severns <severnsc@gmail.com>
2017-02-01 15:10:24 -0500

    Add integration test for header links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:chris)
+  end
+
   test "layout links" do
   	get root_path
   	assert_template 'static_pages/home'
@@ -13,5 +18,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "header links" do
+    get root_path
+    assert_select "a[href=?]", login_path
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
