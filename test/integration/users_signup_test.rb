637b121738067d1a87c4991b0910163f41e33e33
Chris Severns <severnsc@gmail.com>
2017-02-02 10:28:08 -0500

    Update create action to send account activation email and redirect to root on successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,8 +26,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation: "mynewpassword"}}
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert_select 'div.alert-success'
-    assert is_logged_in?
+    #assert_template 'users/show'
+    #assert_select 'div.alert-success'
+    #assert is_logged_in?
   end
 end
