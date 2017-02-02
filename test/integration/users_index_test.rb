4dae81c4b1f6112d13bc3707b1e690c160d69c74
Chris Severns <severnsc@gmail.com>
2017-02-02 12:01:01 -0500

    Test unactivated users index and show pages

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 	def setup
 		@admin = users(:chris)
 		@user = users(:archer)
+		@unactivated = users(:krieger)
 	end
 
 	test "index as admin including pagination and delete links" do
@@ -12,7 +13,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 		get users_path
 		assert_template 'users/index'
 		assert_select 'div.pagination'
-		first_page_of_users = User.paginate(page: 1)
+		first_page_of_users = User.where(activated: true).paginate(page: 1)
 		first_page_of_users.each do |user|
 			assert_select 'a[href=?]', user_path(user), text: user.name
 			unless user == @admin
@@ -28,5 +29,17 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 		log_in_as(@user)
 		get users_path
 		assert_select 'a', text: 'delete', count: 0
+	end
+
+	test "index only shows activated users" do
+		log_in_as(@user)
+		get users_path
+		assert_select 'a[href=?]', user_path(@unactivated), false
+	end
+
+	test "unactivated users shouldn't have show pages" do
+		log_in_as(@user)
+		get user_path(@unactivated)
+		assert_redirected_to root_url
 	end
 end
