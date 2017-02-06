f1b4b4887f9f6ccd1596813b4e062657038a677c
Chris Severns <severnsc@gmail.com>
2017-02-06 16:45:09 -0500

    Setup login restrictions for micropost create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :password_resets, only: [:new, :create, :edit, :update]
 
+  resources :microposts, only: [:create, :destroy]
+
   get '/login', to: 'sessions#new'
 
   post '/login', to: 'sessions#create'
